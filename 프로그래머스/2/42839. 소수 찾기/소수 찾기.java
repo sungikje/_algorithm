178bebae71631797ca53385076eb3b7c5a897309
sung-ik-je <96014470+sung-ik-je@users.noreply.github.com>
2023-12-05 15:51:02 +0900

    [level 2] Title: 소수 찾기, Time: 10.36 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,68 @@
+import java.util.*;
+class Solution {
+    
+    static List<Integer> finalResult = new ArrayList<>();	
+    
+    public int solution(String numbers) {
+        int answer = 0;
+        String[] numbersEmpty = numbers.split("");
+        int[] arr = new int[numbersEmpty.length];
+        int[] result = new int[arr.length];
+		boolean[] visited = new boolean[arr.length];
+        
+        
+        for(int i = 0; i<numbersEmpty.length; i++){
+            arr[i] = Integer.parseInt(numbersEmpty[i]);
+        }
+        
+        for(int i = 0; i<arr.length; i++) {
+			permutation(arr, result, visited, 0, i+1);
+		}
+        // System.out.println(answer);
+        answer = finalResult.size();
+        
+        return answer;
+    }
+    static void permutation(int[] arr, int[] result, boolean[] visited, int depth, int r) {
+		
+		if(depth == r) {
+			String checkResult = "";
+			for(int i =0; i<r; i++) {
+				checkResult += Integer.toString(result[i]);
+			}
+			if(prime(Integer.parseInt(checkResult)) && !(finalResult.contains(Integer.parseInt(checkResult)))) {
+				
+				// System.out.println("소수 : "+checkResult);
+				finalResult.add(Integer.parseInt(checkResult));
+				return;
+			}
+		}
+		
+		
+		
+		for(int i =0; i<arr.length; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				result[depth] = arr[i];
+				permutation(arr, result, visited, depth+1,r);
+				visited[i] = false;
+			}
+		}
+	}
+	
+	
+	static boolean prime(int num) {
+		
+		if(num >= 2) {
+			for(int i = 2; i< num; i++) {
+				if(num % i == 0) {
+					return false;
+				}
+			}
+		}
+		else {
+			return false;
+		}
+		return true;
+	}
+}
