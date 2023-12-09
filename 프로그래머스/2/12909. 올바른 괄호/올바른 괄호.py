2280502ea668d46e221e13fd79c2947472237d3a
sung-ik-je <96014470+sung-ik-je@users.noreply.github.com>
2023-12-09 09:41:26 +0900

    [level 2] Title: 올바른 괄호, Time: 8.99 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,20 @@
+def solution(s):
+    answer = True
+    
+    stack = []
+        
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        else:
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+    
+    
+    
