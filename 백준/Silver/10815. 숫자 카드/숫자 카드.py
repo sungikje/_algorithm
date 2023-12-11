463220230adc6b710b7da6bdef9f8ee888bfb52e
sung-ik-je <96014470+sung-ik-je@users.noreply.github.com>
2023-12-11 14:22:18 +0900

    [Silver V] Title: 숫자 카드, Time: 2232 ms, Memory: 113352 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,28 @@
+n = int(input())
+arrN = list(map(int,input().split(' ')))
+m = int(input())
+arrM = list(map(int,input().split(' ')))
+arrN.sort()
+
+
+
+def find_number(n, arr, start, end):
+
+    if start > end:
+        print(0,end=' ')
+        return False
+
+    mid = (start+end)//2
+
+    if arr[mid] > n:
+        find_number(n, arr, start, mid-1)
+    elif arr[mid] < n :
+        find_number(n, arr, mid+1, end)
+    else:
+        print(1,end=' ')
+        return True
+
+
+
+for i in arrM:
+    find_number(i, arrN, 0, len(arrN)-1)
