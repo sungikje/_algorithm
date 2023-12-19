0007c348ae399f4fb968723ecd933ea2c86dc363
성익제/Sung Ik Je <96014470+sung-ik-je@users.noreply.github.com>
2023-12-19 17:05:41 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 84 ms, Memory: 12104 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String str = br.readLine();
+
+        while(str != null){
+            System.out.println(str);
+            str = br.readLine();
+        }
+
+    }
+}
