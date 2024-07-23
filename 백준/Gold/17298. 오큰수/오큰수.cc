23d8fe95f4ba447dd2152d68721dfdab44fa557c
성익제/Sung Ik Je <96014470+sung-ik-je@users.noreply.github.com>
2024-07-23 21:46:07 +0900

    [Gold IV] Title: 오큰수, Time: 252 ms, Memory: 10212 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int n;
+  int tmp;
+  cin >> n;
+  vector<int> vec(n);
+  stack<int> stack;
+
+  for(int i = 0; i < n; i++) {
+    cin >> tmp;
+    vec[i] = tmp;
+  }
+
+  // for(int i : vec) cout << i << " ";
+  // cout << endl;
+
+  for(int i = 0; i < n; i++) {
+    if (stack.empty()) {
+      stack.push(i);
+    } else {
+      while(true) {
+        if (stack.empty()) {
+          stack.push(i);
+          break;
+        }
+        if (vec[i] > vec[stack.top()]) {
+          vec[stack.top()] = vec[i];
+          stack.pop();
+        } else {
+          stack.push(i);
+          break;
+        }
+      }
+    }
+  }
+
+  vec[vec.size()-1] = -1;
+  while(!stack.empty()) { 
+    vec[stack.top()] = -1;
+    stack.pop();
+  }
+
+  for(int i : vec) cout << i << " ";
+
+}
+/*
+너무 모든 index부터 value들을 다 저장해서 핸들링하려고 하는 것 같다
+모든 값들을 저장하는 것이 능사는 아니다
+
+*/
