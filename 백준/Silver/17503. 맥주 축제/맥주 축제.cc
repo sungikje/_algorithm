a6be4526ce6c52d470f9b39ea8f19b36b54ea749
sung-ik-je <ij_16@naver.com>
2024-08-17 13:40:15 +0900

    sort vs priority queue

--- 백준/Silver/17503. 맥주 축제/맥주 축제.cc
+++ 백준/Silver/17503. 맥주 축제/맥주 축제.cc
@@ -32,6 +32,14 @@ N일 동안 참가해 N 종류의 맥주를 마시려 함
     도수 기준 오름차순 정렬
     선호도 만족할 때 까지 맥주 추가하면서 확인
   ref : https://jinho9610.tistory.com/36
+
+sort의 경우 less-than, 작다를 기준으로 하기에 a < b : a가 더 작을 수록 우선순위
+priority queue의 경우는 기본적으로 greater-than, 내림차순으로 정렬된 힙을 제공하고 있기 때문에 오름차순으로 정렬하고자 할 땐 a < b의 반대인 a > b 정의 필요
+  sort와는 다르게  a < b : 큰 값 b가 작은 값 a 보다 우선 순위라는 의미, 이는 큰 값이 큐의 우선 순위가 되며 최대 힙을 의미(내림차순)
+
+sort는 less-than, a를 기준으로 a > b, a가 큰 경우 우선 순위 = 내림차순
+priority queue의 경우 greater-than, a를 기준으로 a < b, a보다 큰 수들을 우선 순위 = 내림차순
+  a > b, a 보다 작은 순들을 우선 순위 = 오름차순
 */
 
 #include <iostream>
