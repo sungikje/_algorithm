f0cce0e475e1bab764254155831baa9fcccefa70
sung-ik-je <ij16_21@naver.com>
2025-05-29 21:47:18 +0900

    29754 comment

--- 백준/Silver/29754. 세상에는 많은 유튜버가 있고， 그중에서 버츄얼 유튜버도 존재한다/세상에는 많은 유튜버가 있고， 그중에서 버츄얼 유튜버도 존재한다.cc
+++ 백준/Silver/29754. 세상에는 많은 유튜버가 있고， 그중에서 버츄얼 유튜버도 존재한다/세상에는 많은 유튜버가 있고， 그중에서 버츄얼 유튜버도 존재한다.cc
@@ -62,3 +62,15 @@ int main() {
         }
     }
 }
+
+/*
+초기에는 아래 구조로 풀려고 했었다.
+name
+    period
+        total count, total minute
+이 경우 모든 계층마다 자료 구조를 사용하면 map : vector : pair 순으로 구성될거고 유저 정보가 들어올 때마다 vector를 순회하면서 period(주 차)를 찾아야되는 문제 존재
+=> 너무 비효율적
+
+문제 구조상 최대 100명, 최대 52주 차, total count, minute 해도 int로 커버가 될 것 같아서 array index를 period로 간주하는 방식으로 진행
+100 * (52 * (4 + 4))byte 정도여서 충분히 가능할 것 같다고 판단
+*/
