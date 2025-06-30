8953a625ddeaaf3c010a94187fddd5123ebabb5a
성익제/Sung Ik Je <96014470+sungikje@users.noreply.github.com>
2025-06-30 21:48:17 +0900

    [Silver III] Title: 햄버거 분배, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <array>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int n, k;
+    std::cin >> n >> k;
+    std::array<char, 20000> arr;
+    arr.fill(0);
+    int result = 0;
+
+    for(int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        if (arr[i] == 'P') {
+            int start = i - k >=0 ? i-k : 0;
+            int end = i + k >= n ? n-1 : i + k;
+            bool eat = false;
+
+            for(int j = start; j < i; j++) {
+                if (arr[j] == 'H') {
+                    arr[j] = 'X';
+                    arr[i] = 'X';
+                    eat = true;
+                    result++;
+                    break;
+                }
+            }
+            
+            if(!eat) {
+                for(int j = i+1; j <= end; j++){
+                    if (arr[j] == 'H') {
+                        arr[j] = 'X';
+                        arr[i] = 'X';
+                        result++;
+                        break;
+                    }
+                }   
+            }
+        }
+    }
+
+    std::cout << result;
+}
