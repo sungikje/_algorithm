cb8c4b5221a0647869d87445e429a401fd47b1f2
성익제/Sung Ik Je <96014470+sungikje@users.noreply.github.com>
2025-09-20 06:05:06 +0900

    [Silver I] Title: 숨바꼭질, Time: 388 ms, Memory: 24460 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+void push_q(std::queue<std::vector<int>>& q, std::vector<bool> visited, int loc, int cnt) {
+    int new_loc = loc-1;
+    int new_cnt = cnt+1;
+    if (!visited[new_loc] && 0 <= new_loc && new_loc <= 100000) {
+        q.push({new_loc, new_cnt});
+    }
+    
+    new_loc = loc+1;
+    if (!visited[new_loc] && 0 <= new_loc && new_loc <= 100000) {
+        q.push({new_loc, new_cnt});
+    }
+
+    new_loc = loc*2;
+    if (!visited[new_loc] && 0 <= new_loc && new_loc <= 100000) {
+        q.push({new_loc, new_cnt});
+    }
+}
+
+int main () {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    int n, k;
+    std::cin >> n >> k;
+
+    std::queue<std::vector<int>> q;
+    std::vector<bool> visited(100001, false);
+    q.push({n, 0});
+    while(!q.empty()) {
+        std::vector<int> front = q.front();
+        int loc = front[0], cnt = front[1];
+        visited[loc] = true;
+        q.pop();
+
+        if (loc == k) {
+            std::cout << cnt;
+            return 0;
+        }
+        push_q(q, visited, loc, cnt);
+    }
+}
