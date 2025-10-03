87801e1d3f7b926dfb463d33fef7a7f4ffb4cba0
성익제/Sung Ik Je <96014470+sungikje@users.noreply.github.com>
2025-10-03 10:32:22 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 112 ms, Memory: 10356 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<std::vector<int>> grid(n+1, std::vector<int>(n+1, 0));
+    std::vector<std::vector<int>> prefix(n+1, std::vector<int>(n+1, 0));
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            std::cin >> grid[i][j];
+            prefix[i][j] = grid[i][j] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1];
+        }
+    }
+
+    for(int i = 0; i < m; i++) {
+        int start_x, start_y, end_x, end_y;
+        std::cin >> start_x >> start_y >> end_x >> end_y;
+        int prefix_sum = prefix[end_x][end_y] + prefix[start_x-1][start_y-1] - prefix[start_x-1][end_y] - prefix[end_x][start_y-1];
+        std::cout << prefix_sum << "\n";
+    }
+}
