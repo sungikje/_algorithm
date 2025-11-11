a5460291be141780e7c63c752d105e506177eca5
성익제/Sung Ik Je <96014470+sungikje@users.noreply.github.com>
2025-11-12 06:34:08 +0900

    [Silver II] Title: 촌수계산, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    int n, start, end, cnt;
+    std:: cin >> n >> start >> end >> cnt;
+    std::vector<std::vector<int>> graph(n+1, std::vector<int>(n, 0));
+    std::vector<bool> visited(n+1, false);
+    while(cnt) {
+        int a, b;
+        std::cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+        cnt--;
+    }
+
+    std::queue<std::vector<int>> q;
+    for(auto& a: graph[start]) {
+        q.push({a, 1});
+        visited[a] = true;
+    }
+
+    while (!q.empty()) {
+        std::vector<int> node_info = q.front();
+        int node = node_info[0], depth = node_info[1];
+        q.pop();
+
+        if (node == end) {
+            std::cout << depth;
+            return 0;
+        }
+
+        for(auto& a: graph[node]) {
+            if (!visited[a]) {
+                q.push({a, depth+1});
+                visited[a] = true;
+            }
+        }
+    }
+    std::cout << -1;
+}
