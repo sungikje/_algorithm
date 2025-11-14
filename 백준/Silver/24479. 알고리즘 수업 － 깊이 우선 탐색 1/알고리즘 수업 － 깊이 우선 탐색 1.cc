0ef153d69a57d00458ee2a9189be9062a121261c
성익제/Sung Ik Je <96014470+sungikje@users.noreply.github.com>
2025-11-15 05:28:32 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 80 ms, Memory: 8988 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n, m, r;
+    std::cin >> n >> m >> r;
+
+    std::vector<std::vector<int>> graph(n+1);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        std::cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        std::sort(graph[i].begin(), graph[i].end()); 
+    }
+
+    std::vector<int> res(n+1, 0);
+    std::vector<bool> visited(n+1, false);
+
+    std::stack<int> st;
+    st.push(r);
+    int order = 1;
+
+    while (!st.empty()) {
+        int node = st.top();
+        st.pop();
+
+        if (visited[node]) continue;
+        visited[node] = true;
+
+        res[node] = order++;
+
+        for (int i = graph[node].size() - 1; i >= 0; i--) {
+            int nxt = graph[node][i];
+            if (!visited[nxt]) st.push(nxt);
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        std::cout << res[i] << "\n";
+    }
+}
